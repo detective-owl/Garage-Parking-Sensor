2b052e489fc715fc2235f5a95693dc3f2e742e19
detective-owl <97421454+detective-owl@users.noreply.github.com>
2022-03-26 18:01:27 +0800

    Update Garage_Sensor_With_LED.ino

--- Garage_Sensor_With_LED.ino
+++ Garage_Sensor_With_LED.ino
@@ -10,23 +10,23 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int trigPin = 9;
 const int echoPin = 10;
-long duration;
-int distanceCm;
-float dist;
 
 void setup()
 {
-  lcd.begin(16, 2); // Initializes the interface to the LCD screen, and specifies the dimensions (width and height) of the display
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode (3, OUTPUT);
-  pinMode (4, OUTPUT);
-  pinMode (5, OUTPUT);
-  pinMode (6, OUTPUT);
-  pinMode (7, OUTPUT);
-  pinMode (8, OUTPUT);
-  digitalWrite(3, HIGH);
-
+  lcd.init();
+  lcd.backlight();
+  pinMode( trigPin, OUTPUT );
+  pinMode( echoPin, INPUT );
+  for ( int led = 0;  led < MAX;  ++led )
+  {
+    pinMode( LED_PINS[led], OUTPUT );
+  }
+  digitalWrite( 3, HIGH );
+  lcd.setCursor( 0, 0 );
+  lcd.print( "Parking System" );
+  delay( 500 );
+  lcd.setCursor( 0, 0 );
+  lcd.print( "Ready........." );
 }
 
 // Determine distance to the vehicle & return it in cm.
@@ -49,7 +49,7 @@ void lcdShowDistance( int distanceCm )
   lcd.setCursor(0, 0); // set to LCD upper left corner
   lcd.print("Distance: "); // Prints string "Distance" on the LCD
   lcd.print(distanceCm); // Prints the distance value from the sensor
-  lcd.print(" cm");
+  lcd.print( " cm  " );
   delay(10);
 }
 
@@ -73,14 +73,15 @@ void ledShowDistance( int distanceCm )
   for (int  thresh = 1; thresh < MAX; thresh++)
   {
     if (distanceCm > LED_THRESHOLDS[thresh])
-      return; 
-    {
-      digitalWrite (LED_PINS[thresh], HIGH);
-    }
+      return;
+    digitalWrite (LED_PINS[thresh], HIGH);
   }
 }
 
 // Main Arduino Loop
-void loop() {
-  ledShowDistance( getDistance() );
+void loop()
+{
+  int distance = getDistance();
+  lcdShowDistance( distance );
+  ledShowDistance( distance );
 }
